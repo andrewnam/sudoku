10bad3aed0c1317b17bac3d0fe694d7ebc84e893
Andrew Nam <andrew@Joos-MacBook-Pro.local>
2018-12-13 14:25:18 -0800

    Shuffler done

--- src/sudoku/shuffler.py
+++ src/sudoku/shuffler.py
@@ -8,6 +8,7 @@ import itertools
 from shuffled_grid import ShuffledGrid
 from grid_string import GridString
 
+
 class Shuffler:
     def __init__(self, grid_string: GridString):
         self.grid_string = grid_string
@@ -18,4 +19,3 @@ class Shuffler:
         permutations = [{i + 1: tupl[i] for i in range(len(tupl))} for tupl in perm_tuples]
         shuffled_grids = [ShuffledGrid(self.grid_string, perm) for perm in permutations]
         return shuffled_grids
-    
